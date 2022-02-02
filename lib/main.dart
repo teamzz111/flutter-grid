0fc6fd3b8ae246284d337833398aa4fb1b22de83
teamzz111 <felipeflogxd@gmail.com>
2022-02-02 02:00:57 -0500

    Lato for dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
+        fontFamily: 'Lato'
       ),
       home: Scaffold(appBar: AppBar(
         title: const Text("Testing Flutter"),

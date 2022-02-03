d2a1df1e788204900aa010bb6888791136eccfee
teamzz111 <felipeflogxd@gmail.com>
2022-02-03 01:45:17 -0500

    transparent android

--- android/app/src/main/kotlin/com/example/grid/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/grid/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.example.grid
 
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
+    }
 }
